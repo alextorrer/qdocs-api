196adc2fb6bccc7d5cbe10eb77c8496107ec0e10
alextorrer <alextorre0427@gmail.com>
2022-12-09 17:22:17 -0600

    Send role on login

--- controllers/auth.js
+++ controllers/auth.js
@@ -66,6 +66,7 @@ const sendTokenResponse = (user, statusCode, res) => {
         .cookie('token', token, options)
         .json({
             success: true,
-            token
+            token,
+            role: user.role
         });
 }
